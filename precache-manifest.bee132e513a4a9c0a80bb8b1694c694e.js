fd8732d905f4d7a183d3475955f7291a13e9fb2d
Tom Sanderson <tom.w.j.sanderson@gmail.com>
2020-04-30 00:22:24 -0400

    Updates

---
+++ precache-manifest.bee132e513a4a9c0a80bb8b1694c694e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5babd49adbd282a1494b45901ad425dc",
+    "url": "/foodFinder/index.html"
+  },
+  {
+    "revision": "08b2912313526ea23fad",
+    "url": "/foodFinder/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "945def1b483bdd3c6257",
+    "url": "/foodFinder/static/css/main.62f7a6f9.chunk.css"
+  },
+  {
+    "revision": "08b2912313526ea23fad",
+    "url": "/foodFinder/static/js/2.ceef306a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/foodFinder/static/js/2.ceef306a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "945def1b483bdd3c6257",
+    "url": "/foodFinder/static/js/main.1d00b7b3.chunk.js"
+  },
+  {
+    "revision": "b047a38da4deceb3e86f",
+    "url": "/foodFinder/static/js/runtime-main.98035c08.js"
+  }
+]);
